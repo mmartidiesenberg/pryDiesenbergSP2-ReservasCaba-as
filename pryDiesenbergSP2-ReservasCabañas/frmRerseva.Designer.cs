8e3a1448d2102fd651b31c6813157bd9d265368f
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2025-09-26 10:03:11 -0300

    col

--- pryDiesenbergSP2-ReservasCabañas/frmRerseva.Designer.cs
+++ pryDiesenbergSP2-ReservasCabañas/frmRerseva.Designer.cs
@@ -295,6 +295,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(546, 450);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.mrcTitular);
